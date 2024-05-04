24c18ff59cec3a4365e7718428fbf43eb8ae4633
YuYangWoo <yuyw0712@naver.com>
2024-05-04 22:34:30 +0900

    Programmers - 대충 만든 자판 풀이

---
+++ src/Programmers/대충 만든 자판.kt
@@ -0,0 +1,35 @@
+package Programmers
+
+fun main() {
+    solution(arrayOf("BACDE", "ABCD"), arrayOf("ABCDW", "FGHIJ", "WA", )).forEach { println(it) }
+}
+
+fun solution(keymap: Array<String>, targets: Array<String>): MutableList<Int> {
+    var answer = mutableListOf<Int>()
+    val minKeyMap = HashMap<Char, Int>()
+    buildMinKey(keymap, minKeyMap)
+
+    targets.forEach { target ->
+        var count = 0
+        var flag = false
+        target.forEach {
+            if (minKeyMap.containsKey(it) && !flag) {
+                count += minKeyMap[it]!!
+            } else {
+                flag = true
+                count = -1
+            }
+        }
+        answer.add(count)
+    }
+
+    return answer
+}
+
+fun buildMinKey(keymap: Array<String>, minKeyMap: HashMap<Char, Int>) {
+    keymap.forEach { key ->
+        key.forEachIndexed { index, literal ->
+            minKeyMap[literal] = minOf(minKeyMap[literal] ?: (index + 1), index + 1)
+        }
+    }
+}
