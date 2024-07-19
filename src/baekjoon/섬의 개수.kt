35eb982e8c6eedc59c09785dca544be9cc987b2e
YuYangWoo <yuyw0712@naver.com>
2024-07-19 23:31:01 +0900

    baekjoon - 섬의 개수 풀이

--- src/baekjoon/섬의 개수.kt
+++ src/baekjoon/섬의 개수.kt
@@ -2,49 +2,48 @@ package baekjoon
 
 private val dir = arrayOf(
     0 to 1,
-    -1 to 1,
-    -1 to 0,
-    -1 to -1,
     0 to -1,
-    1 to -1,
     1 to 0,
+    -1 to 0,
     1 to 1,
+    -1 to 1,
+    -1 to -1,
+    1 to -1
 )
-fun main() = with(System.`in`.bufferedReader()) {
-    while(true) {
-        val (row,col) = readLine()!!.split(" ").map { it.toInt() }
-        if (row == 0 && col == 0) break
 
+fun main() = with(System.`in`.bufferedReader()) {
+    while (true) {
+        val (row, col) = readLine().split(" ").map { it.toInt() }
+
+        if (row == 0 && col == 0) break
         val map = Array(col) { mutableListOf<Int>() }
-        val isVisited = Array(col) { BooleanArray(row) }
+        val adjacent = Array(col) { BooleanArray(row) }
 
         repeat(col) {
-            map[it] = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+            map[it] = readLine().split(" ").map { it.toInt() }.toMutableList()
         }
 
         var count = 0
-        for (i in 0 until col) {
-            for (j in 0 until row) {
-                if (!isVisited[i][j] && map[i][j] == 1) {
-                    dfs(i, j, map, isVisited, row, col)
+        for (i in map.indices) {
+            for (j in map[i].indices) {
+                if (adjacent[i][j].not() && map[i][j] == 1) {
+                    dfs(i, j, map, adjacent)
                     count++
                 }
             }
         }
         println(count)
     }
-
 }
 
-fun dfs(sero: Int, garo: Int, map: Array<MutableList<Int>>, visited: Array<BooleanArray>, row: Int, col: Int) {
-    visited[sero][garo] = true
-
+private fun dfs(col: Int, row: Int, map: Array<MutableList<Int>>, adjacent: Array<BooleanArray>) {
+    adjacent[col][row] = true
     for (dir in dir) {
-        val dirGaro = garo + dir.first
-        val dirSero = sero + dir.second
+        val dirRow = row + dir.first
+        val dirCol = col + dir.second
 
-        if (dirGaro >= 0 && dirSero >= 0 && dirGaro < row && dirSero < col && !visited[dirSero][dirGaro] && map[dirSero][dirGaro] == 1) {
-            dfs(dirSero, dirGaro, map, visited, row, col)
+        if (dirRow >= 0 && dirCol >= 0 && dirRow < map[0].size && dirCol < map.size && map[dirCol][dirRow] == 1 && adjacent[dirCol][dirRow].not()) {
+            dfs(dirCol, dirRow, map, adjacent)
         }
     }
 }
